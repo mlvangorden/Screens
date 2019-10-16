3d209c2f6057bea03ee5a5bc85c892a4bd7c6475
Matthew Van Gorden <33157785+mlvangorden@users.noreply.github.com>
2019-10-16 10:41:09 -0400

    10/16/19 10:40am Setup basic outline for game, made pig class

--- Screens/Screens/GameScene.swift
+++ Screens/Screens/GameScene.swift
@@ -8,103 +8,88 @@
 
 import SpriteKit
 import GameplayKit
+import AVFoundation
 
-class GameScene: SKScene {
-    
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func sceneDidLoad() {
+var screenBounds = CGRect()
+var screen_width = CGFloat()
+var screen_height = CGFloat()
 
-        self.lastUpdateTime = 0
+struct category {
+    static let pig : UInt32 = 0x4
+    static let bomb : UInt32 = 0x3
+    static let hedgehog : UInt32 = 0x2
+    static let garden : UInt32 = 0x1
+}
+
+class GameScene: SKScene, SKPhysicsContactDelegate {
+    
+    override func didMove(to view: SKView) {
+        screenBounds = UIScreen.main.bounds
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let temp_height = screenBounds.height
+        let temp_width = screenBounds.width
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        screen_width = max(temp_height, temp_width)
+        screen_height = min(temp_height, temp_width)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        self.physicsWorld.contactDelegate = self
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+    func didBegin(_ contact: SKPhysicsContact) {
+
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+    @objc func spawnPig(){
+        let pig = Pig()
+        self.addChild(pig)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        for touch in touches {
+            let location = touch.location(in: self)
+            
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+        for touch in touches {
+            let location = touch.location(in: self)
+            let previous_location = touch.previousLocation(in: self)
+            
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
+        for touch in touches {
+            let previous_location = touch.previousLocation(in: self)
+            
         }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
     }
+    
+    override func update(_ currentTime: TimeInterval){
+
+    }
+}
+
+class Pig : SKSpriteNode {
+    var health = Int()
+    
+    init() {
+        let texture = SKTexture(imageNamed: "pig")
+        super.init(texture: texture, color: UIColor.clear, size: texture.size())
+        health = 5
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func takeDamage() {
+        health -= 1
+    }
+    
+    func death() {
+        self.removeFromParent()
+    }
+    
 }
--- Screens/Screens/GameViewController.swift
+++ Screens/Screens/GameViewController.swift
@@ -48,9 +48,9 @@ class GameViewController: UIViewController {
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
+            return .portrait
         } else {
-            return .all
+            return .portrait
         }
     }
 
