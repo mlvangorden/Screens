48bd6b0321227cb8dd7930d5b8db6782b791e369
Matthew Van Gorden <33157785+mlvangorden@users.noreply.github.com>
2019-10-23 21:36:29 -0400

    10/23/19 9:36pm Started with title screen/play button

--- Screens/Screens/GameScene.swift
+++ Screens/Screens/GameScene.swift
@@ -23,7 +23,7 @@ let arrow_dimensions : CGFloat = 168.25
 
 var button_directions = [false, false, false, false]
 var board = [Space]()
-var arrows = [SKSpriteNode]()
+var arrows = [Arrow]()
 
 class GameScene: SKScene {
     
@@ -33,6 +33,12 @@ class GameScene: SKScene {
     var player = Player(space: 0)
     var enemy1 = Enemy(space: 0)
     var enemy2 = Enemy(space: 0)
+    var star = Star(space: 0)
+    
+    var score = 0
+    var score_display = SKLabelNode()
+    
+    var waiting_for_move : Bool = false
     
     private var lastUpdateTime : TimeInterval = 0
     
@@ -63,13 +69,33 @@ class GameScene: SKScene {
     }
     
     func startGame() {
-        player = Player(space: 31)
+        player = Player(space: 27)
         enemy1 = Enemy(space: 0)
         enemy2 = Enemy(space: 3)
         self.addChild(player)
         self.addChild(enemy1)
         self.addChild(enemy2)
+        
+        score_display.fontName = "Times"
+        score_display.horizontalAlignmentMode = .center
+        score_display.position = CGPoint(x: screen_width / 2, y: -125)
+        score_display.zPosition = 1
+        score_display.fontColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+        score_display.fontSize = 80
+        score_display.text = String(score)
+        self.addChild(score_display)
+        
+        spawnStar()
         updateButtons()
+    }
+    
+    func restartGame() {
+        player.removeFromParent()
+        enemy1.removeFromParent()
+        enemy2.removeFromParent()
+        score_display.removeFromParent()
+        score = 0
+        startGame()
     }
     
     func createBoard() {
@@ -118,38 +144,10 @@ class GameScene: SKScene {
     
     func addArrows() {
         for i in 0...3 {
-            var image_name : String
-            switch i {
-            case 0:
-                image_name = "arrow_nw"
-            case 1:
-                image_name = "arrow_sw"
-            case 2:
-                image_name = "arrow_se"
-            case 3:
-                image_name = "arrow_ne"
-            default:
-                image_name = "arrow_nw"
-            }
-            let arrow_button = SKSpriteNode(imageNamed: image_name)
-            arrow_button.anchorPoint = CGPoint(x: 0, y: 1)
-            arrow_button.name = image_name
-            
-            var x_position : CGFloat = (screen_width / 2) - (button_offset / 2) - arrow_dimensions
-            var y_position : CGFloat = -1 * (screen_height - button_offset - arrow_dimensions)
-            
-            if(i % 3 == 0) {
-                y_position += (button_offset + arrow_dimensions)
-            }
-            if(i > 1) {
-                x_position += (button_offset + arrow_dimensions)
-            }
-            
-            arrow_button.position = CGPoint(x: x_position, y: y_position)
+            let arrow = Arrow(direction: i)
 
-            arrows.append(arrow_button)
-            self.addChild(arrow_button)
-            
+            arrows.append(arrow)
+            self.addChild(arrow)
         }
     }
     
@@ -157,38 +155,61 @@ class GameScene: SKScene {
         for i in 0...3 {
             let arrow = arrows[i]
             if (player.getNeighbor(direction: i) == -1) {
-                arrow.texture = SKTexture(imageNamed: arrow.name! + "2")
+                arrow.deactivate()
             } else {
-                arrow.texture = SKTexture(imageNamed: arrow.name!)
+                arrow.activate()
             }
         }
     }
     
+    func spawnStar() {
+        star.removeFromParent()
+        var random_int = Int.random(in: 0..<32)
+        
+        while  ( random_int == player.get_position() || random_int == enemy1.get_position() || random_int == enemy2.get_position() || enemy1.checkNeighbor(neighbor: random_int) || enemy2.checkNeighbor(neighbor: random_int) || player.checkNeighbor(neighbor: random_int) ) {
+            random_int = Int.random(in: 0..<32)
+        }
+        star = Star(space: random_int)
+        self.addChild(star)
+    }
+    
+    func scoreStar(){
+        score += 1
+        score_display.text = String(score)
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            let location = touch.location(in: self)
-            checkButtonPress(location: location)
+        if(!waiting_for_move){
+            for touch in touches {
+                let location = touch.location(in: self)
+                checkButtonPress(location: location)
+            }
         }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            let location = touch.location(in: self)
-            let previous_location = touch.previousLocation(in: self)
-            checkButtonPress(location: location)
+        if(!waiting_for_move) {
+            for touch in touches {
+                let location = touch.location(in: self)
+                let previous_location = touch.previousLocation(in: self)
+                checkButtonPress(location: location)
+            }
         }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            let previous_location = touch.previousLocation(in: self)
+        if(!waiting_for_move) {
+            for touch in touches {
+                let previous_location = touch.previousLocation(in: self)
                 movePlayer()
+                button_directions = [false, false, false, false]
             }
+        }
     }
     
     func checkButtonPress(location: CGPoint) {
         for i in 0...3 {
-            if (arrows[i].contains(location)) {
+            if (arrows[i].contains(location) && arrows[i].active) {
                 button_directions[i] = true
             } else {
                 button_directions[i] = false
@@ -200,15 +221,58 @@ class GameScene: SKScene {
         for i in 0...3 {
             if(button_directions[i]) {
                 player.move_direction(direction: i)
-                player.updateNeighbors()
                 updateButtons()
-                return
+                moveEnemies()
+                waiting_for_move = true
+                break
             }
+        }
+    }
+    
+    func moveEnemies() {
+        for enemy in [enemy1, enemy2] {
+            var possible_directions = [false, false, false, false]
+            for i in 0...3 {
+                if (enemy.getNeighbor(direction: i) != -1) {
+                    possible_directions[i] = true
+                }
+                if (enemy.getNeighbor(direction: i) == star.get_position() || enemy.getNeighbor(direction: i) == enemy1.get_position()) {
+                    possible_directions[i] = false
+                }
+            }
+            if(possible_directions == [false, false, false, false]) {
+                for i in 0...3 {
+                    if ( enemy.getNeighbor(direction: i) == enemy1.get_position() ) {
+                        possible_directions[i] = true
+                    }
+                }
+            }
+            
+            var random_direction = Int.random(in: 0..<4)
+            while (possible_directions[random_direction] == false) {
+                random_direction = Int.random(in: 0..<4)
+            }
+            enemy.move_direction(direction: random_direction)
         }
     }
     
     override func update(_ currentTime: TimeInterval){
         // Called before each frame is rendered
+        
+        if(player.position == board[player.get_position()].position) {
+            waiting_for_move = false
+        }
+        
+        //losing condition
+        if(player.position == enemy1.position || player.position == enemy2.position) {
+            restartGame()
+        }
+        //scoring condition
+        else if (player.position == star.position) {
+            scoreStar()
+            spawnStar()
+        }
+        
         
         // Initialize _lastUpdateTime if it has not already been
         if (self.lastUpdateTime == 0) {
@@ -290,6 +354,86 @@ class Space : SKSpriteNode {
     }
 }
 
+class Arrow : SKSpriteNode {
+    var active : Bool = true
+    var dir : Int = -1
+    
+    init(direction: Int) {
+        dir = direction
+        var image_name : String
+        switch dir {
+        case 0:
+            image_name = "arrow_nw"
+        case 1:
+            image_name = "arrow_sw"
+        case 2:
+            image_name = "arrow_se"
+        case 3:
+            image_name = "arrow_ne"
+        default:
+            image_name = "arrow_nw"
+        }
+        let texture = SKTexture(imageNamed: image_name)
+        super.init(texture: texture, color: UIColor.clear, size: texture.size())
+        self.anchorPoint = CGPoint(x: 0, y: 1)
+        self.name = image_name
+        self.zPosition = 1
+        
+        var x_position : CGFloat = (screen_width / 2) - (button_offset / 2) - arrow_dimensions
+        var y_position : CGFloat = -1 * (screen_height - button_offset - arrow_dimensions)
+        
+        if(dir % 3 == 0) {
+            y_position += (button_offset + arrow_dimensions)
+        }
+        if(dir > 1) {
+            x_position += (button_offset + arrow_dimensions)
+        }
+        self.position = CGPoint(x: x_position, y: y_position)
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func deactivate() {
+        var image_name : String
+        switch dir {
+        case 0:
+            image_name = "arrow_nw2"
+        case 1:
+            image_name = "arrow_sw2"
+        case 2:
+            image_name = "arrow_se2"
+        case 3:
+            image_name = "arrow_ne2"
+        default:
+            image_name = "arrow_nw2"
+        }
+        self.texture = SKTexture(imageNamed: image_name)
+        self.name = image_name
+        active = false
+    }
+    
+    func activate() {
+        var image_name : String
+        switch dir {
+        case 0:
+            image_name = "arrow_nw"
+        case 1:
+            image_name = "arrow_sw"
+        case 2:
+            image_name = "arrow_se"
+        case 3:
+            image_name = "arrow_ne"
+        default:
+            image_name = "arrow_nw2"
+        }
+        self.texture = SKTexture(imageNamed: image_name)
+        self.name = image_name
+        active = true
+    }
+}
+
 class Character : SKSpriteNode {
     
     var pos : Int
@@ -316,7 +460,8 @@ class Character : SKSpriteNode {
             let temp = getNeighbor(direction: direction)
             if(temp != -1) {
                 pos = temp
-                self.run(SKAction.move(to: board[pos].position, duration: 0.25))
+                self.run(SKAction.move(to: board[pos].position, duration: 0.2))
+                updateNeighbors()
             }
         }
     }
@@ -330,6 +475,15 @@ class Character : SKSpriteNode {
             return board[pos].getNeighbor(direction: direction)
         }
         return -1
+    }
+    
+    func checkNeighbor(neighbor: Int) -> Bool {
+        for n in neighbors {
+            if (n == neighbor) {
+                return true
+            }
+        }
+        return false
     }
     
     func updateNeighbors() {
@@ -357,6 +511,19 @@ class Enemy : Character {
         let texture = SKTexture(imageNamed: "enemy")
         self.texture = texture
         self.name = "Enemy"
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+class Star : Character {
+    override init(space: Int) {
+        super.init(space: space)
+        let texture = SKTexture(imageNamed: "star")
+        self.texture = texture
+        self.name = "Star"
     }
     
     required init(coder aDecoder: NSCoder) {
---
+++ Screens/Screens/SplashViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SplashViewController.swift
+//  Screens
+//
+//  Created by Matthew Van Gorden on 10/23/19.
+//  Copyright © 2019 Matthew Van Gorden. All rights reserved.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
